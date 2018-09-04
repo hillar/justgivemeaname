bc02d45420d288f7419615ab98ac1844149e113e
Hillar Aarelaid <hillar@aarelaid.net>
2018-09-04 16:14:20 +0300

    isString

--- dev/src/server/utils/var.mjs
+++ dev/src/server/utils/var.mjs
@@ -3,3 +3,7 @@ export function trimArrayOfStrings(a) {
     return [ ...( new Set( a.map( s => ( s.trim().length > 0 ) ? s.trim() : undefined ) ) ) ].filter( i => (!(i === undefined)) )
 
 }
+
+export function isString(s) {
+  return Object.prototype.toString.call( s ) === '[object String]'
+}
--- dev/tests/server/utils/var.tape.mjs
+++ dev/tests/server/utils/var.tape.mjs
@@ -1,14 +1,23 @@
 
 
 import test from 'tape'
-import { trimArrayOfStrings } from '../../../src/server/utils/var.mjs'
+import { trimArrayOfStrings, isString } from '../../../src/server/utils/var.mjs'
 test('var.mjs', function (t) {
   
+  // trimArrayOfStrings
   t.deepEqual(trimArrayOfStrings(['']),[],'empty')
   t.deepEqual(trimArrayOfStrings([' ']),[],'space')
   t.deepEqual(trimArrayOfStrings([' ','  ']),[],'double space')  
   t.deepEqual(trimArrayOfStrings([' a','a  ', ' a ']),[ 'a' ],'string trim')
   t.deepEqual(trimArrayOfStrings(['a','b']),[ 'a', 'b' ],'string trim')
+  t.throws(() => {trimArrayOfStrings()},'throws on not array')
+  t.throws(() => {trimArrayOfStrings([1])},'throws on not string')
+  
+  //isString
+  t.deepEqual(isString(),false,'undefined')
+  t.deepEqual(isString(1),false,'number')
+  t.deepEqual(isString(''),true,'empty string')
+  t.deepEqual(isString('abc'),true,'string')
 
   t.end()
   
