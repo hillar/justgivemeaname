9eedf29f0b5d29ebeaaf6ec05711e0d1896a2fee
HILLAR AARELAID <hillar.aarelaid@politsei.ee>
2018-11-12 15:39:18 +0200

    log upload || download filename

--- dev/src/server/classes/routes/proxy.mjs
+++ dev/src/server/classes/routes/proxy.mjs
@@ -21,7 +21,7 @@ export class ProxyRoute extends Route {
     this.get =  this.post = async (req, res, user, log) => {
         const result = await new Promise( (resolve) => {
           req.pause()
-          let pe = parse(decodeURIComponent(req.url))
+          let pe = parse(req.url)
           pe.dir = pe.dir.replace('/'+this.route,'/')
           const url = join(pe.dir, pe.base)
           log.log_info({proxy:{method:req.method,host:this.host, port:this.port, url}})
@@ -29,7 +29,8 @@ export class ProxyRoute extends Route {
             hostname: this.host,
             port: this.port,
             path: url,
-            method: req.method
+            method: req.method,
+            headers: req.headers
           }
           const proxy = request(options, function (r) {
             r.on('error', (e) => {
@@ -37,13 +38,23 @@ export class ProxyRoute extends Route {
               console.error(e)
               resolve(true)
             })
+            //r.on('data', (c) => {debug('r',c.toString())})
             if (r.statusCode != 200) {
               log.log_err({proxyerror:{status:r.statusCode,host:options.hostname}})
               res.writeHead(500)
               res.end()
               resolve(true)
             }
-            else r.pipe(res)
+            else  {
+              for (const h of Object.keys(r.headers)) {
+                res.setHeader(h,r.headers[h])
+                if ( h === 'content-disposition' ) {
+                  log.log_info({proxy:{downloaded:r.headers[h].split('=').pop(), method:req.method,host:this.host, port:this.port, url}})
+                }
+                //debug(h,' -- ',r.headers[h])
+              }
+              r.pipe(res)
+            }
           })
           proxy.on('error', (e) => {
             log.log_info({proxy:e})
@@ -58,11 +69,32 @@ export class ProxyRoute extends Route {
           res.on('finish', (e) => {
             resolve(true)
           })
+          //res.on('data', (c) => {debug('res',c.toString())})
           req.on('error', (e) => {
             log.log_info({req:e})
             console.error(e)
             resolve(true)
           })
+
+          if (req.method === 'POST') {
+            let data = []
+            req.on('data', (c) => {
+              data += c
+              debug(req.method,c.toString().length)
+            })
+            req.on('end', () => {
+              try {
+                const d = JSON.parse(data.toString())
+                if (d.files) {
+                  for ( const f of d.files) {
+                    log.log_info({proxy:{uploaded:f.filename, method:req.method,host:this.host, port:this.port, url}})
+                  }
+                }
+              } catch (e) {
+                debug(e)
+              }
+            })
+          }
           req.pipe(proxy)
           req.resume()
         })
